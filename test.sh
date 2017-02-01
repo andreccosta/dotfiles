14f6b9769d329f8a2731e9faa1a5bb0d9575f4d0
André Costa <andreccosta@me.com>
2017-02-01 22:44:23 +0000

    Add shellcheck tests

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+(
+# find all executables and run `shellcheck`
+for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
+    if file "$f" | grep --quiet shell; then
+        shellcheck "$f" && echo "[OK]: sucessfully linted $f"
+    fi
+done
+)
