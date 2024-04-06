e81512c964a978c6e2e088402e7a6f1598168a4e
André Costa <andreccosta@me.com>
2024-04-07 00:50:49 +0100

    chore(shellcheck): make it fail

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
-set -e
-set -o pipefail
+#!/usr/bin/env bash
 
-(
+set -eo pipefail
+
+any_failed=false
+
 # find all executables and run `shellcheck`
 for f in $(find . -type f -not -iwholename '*.git*' | sort -u); do
     if file "$f" | grep --quiet shell; then
-        shellcheck "$f" && echo "[OK]: sucessfully linted $f"
+        shellcheck "$f" && echo "[OK]: sucessfully linted $f" || any_failed=true
     fi
 done
-)
+
+if $any_failed; then 
+  exit 1
+fi
