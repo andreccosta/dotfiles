334271f683fa9afd52c4207a6bd3aef8bb366b54
André Costa <andreccosta@me.com>
2025-06-12 20:31:36 +0100

    chore(mac): update defaults

--- mac/set_defaults.sh
+++ mac/set_defaults.sh
@@ -1,15 +1,41 @@
-# hide Safari bookmark bar
+# Hide Safari bookmark bar
 defaults write com.apple.Safari ShowFavoritesBar -bool false
 
-# set Safari to dev mode
+# Set Safari to dev mode
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
-# always open Finder in list view
+# Always open Finder in list view
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# use AirDrop over every interface
+# Use AirDrop over every interface
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+# Fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# Require password immediately after display sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Set Dock icon size to 36 pixels
+defaults write com.apple.dock tilesize -int 48
+
+# Set Dock minimize effect to scale
+defaults write com.apple.dock mineffect -string "scale"
+
+# Auto-hide the Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -int 0
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+
+# Speedup dock animations
+defaults write com.apple.dock springboard-show-duration -float .1
+defaults write com.apple.dock springboard-page-duration -float .2
+
+# Increase file limits
+sudo sysctl kern.maxfiles=64000 kern.maxfilesperproc=28000
