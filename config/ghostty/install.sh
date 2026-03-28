9095aa6f6a2c8f2e0ef90474ad242a24ceed2722
André Costa <andreccosta@me.com>
2026-03-28 15:38:24 +0000

    chore(ghostty): install script

--- config/ghostty/install.sh
+++ config/ghostty/install.sh
@@ -2,5 +2,10 @@
 
 set -euo pipefail
 
-mkdir -p ~/.config/ghostty
-ln -snf "$PWD"/config ~/.config/ghostty/config
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TARGET_DIR="$HOME/.config/ghostty"
+
+mkdir -p "$TARGET_DIR"
+
+ln -snf "$SCRIPT_DIR/config" "$TARGET_DIR"
+ln -snf "$SCRIPT_DIR/shaders" "$TARGET_DIR"
