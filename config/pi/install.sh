98d5c3bcdab126f04b6b3cf828c3b615adb9d39c
André Costa <andreccosta@me.com>
2026-04-04 17:29:06 +0100

    chore(pi): drop default agents

--- config/pi/install.sh
+++ config/pi/install.sh
@@ -9,7 +9,6 @@ mkdir -p "$TARGET_DIR"
 
 ln -snf "$SCRIPT_DIR/agent/extensions" "$TARGET_DIR/extensions"
 ln -snf "$SCRIPT_DIR/agent/themes" "$TARGET_DIR/themes"
-ln -snf "$SCRIPT_DIR/agent/AGENTS.md" "$TARGET_DIR"
 ln -snf "$SCRIPT_DIR/agent/settings.json" "$TARGET_DIR"
 ln -snf "$SCRIPT_DIR/agent/models.json" "$TARGET_DIR"
 ln -snf "$SCRIPT_DIR/agent/package.json" "$TARGET_DIR"
